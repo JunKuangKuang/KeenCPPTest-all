71694cd1acde7440bc3f58add8dc6251d6c3e658
君匡 <1533859388@qq.com>
2022-08-23 23:29:24 +0800

    双端队列的测试

--- STL/deque/deque_test.cpp
+++ STL/deque/deque_test.cpp
@@ -8,22 +8,38 @@
 using namespace std;
 
 int main() {
+    // 定义
     deque<int> deque1{1, 2, 3, 4, 5};
+
     //增加函数
     deque1.push_front(3);//头部增加一个元素X
     deque1.push_back(2);//尾部增加一个元素x
+
+    //查询
     if (!deque1.empty()) { //若true,则deque中无元素
-        //顺序遍历
+        //顺序遍历，跟数组一样可以直接取值！
         for (int i = 0; i < deque1.size(); i++)
             cout << deque1[i] << " ";
+        cout << endl;
 
+        //正向迭代器遍历
+        for (deque<int>::iterator iter = deque1.begin(); iter != deque1.end(); iter++)
+            cout << *iter << " ";
+        cout << endl;
+
+        //反向迭代器遍历
+        for (deque<int>::reverse_iterator iter = deque1.rbegin(); iter != deque1.rend(); iter++)
+            cout << *iter << " ";
         cout << endl;
     }
-    //	deque1.pop_front();//去头
-    //	deque1.pop_back();//去尾
-    //	deque1.clear();//清空
-    ////	cout << num << endl;
+
+    // 删除
+    /*deque1.pop_front();//去头
+    deque1.pop_back();//去尾
+    deque1.clear();//清空*/
+    // 迭代器排序
     sort(deque1.begin(), deque1.end());
+
     if (!deque1.empty()) { //若true,则deque中无元素
         //顺序遍历
         for (int i = 0; i < deque1.size(); i++)
@@ -32,16 +48,6 @@ int main() {
     }
 
     cout << endl;
-    ////删除函数
-    //	deque1.pop_front(): 删除双端队列中最前一个元素
-    //		deque1.pop_back(): 删除双端队列中最后一个元素
-    //		deque1.clear(): 清空双端队列中元素
-    //
-    ////判断函数
-    //		deque1.empty() : 向量是否为空，若true, 则向量中无元素
-    //
-    ////大小函数
-    //		deque1.size(): 返回向量中元素的个数
-    //	cout << deque1[0] << deque1[1];
+
     return 0;
 }
