c04dd46619346bb40bbdab22b42959aa2f88616e
君匡 <1533859388@qq.com>
2022-08-29 20:41:00 +0800

    输出bool类型字段

---
+++ basic/bool/coutBoolalpha.cpp
@@ -0,0 +1,11 @@
+//
+// Created by keen on 2022/8/28.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    bool a=true,b=false;
+    cout<<"a="<<a<<" , b="<<b<<endl;
+    cout<<"a="<<boolalpha<<a<<" , b="<<b<<endl;
+    return 0;
+}
